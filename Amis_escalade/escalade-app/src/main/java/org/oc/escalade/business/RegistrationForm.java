805e0bf303fbf6aca4555cc1efc002d3b5fe5137
mimi <lam99@hotmail.fr>
2019-11-29 10:44:53 +0100

    Add username parameters in RegistrationServlet. Create Registration form in business package

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/RegistrationForm.java
@@ -0,0 +1,4 @@
+package org.oc.escalade.business;
+
+public class RegistrationForm  {
+}
--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/RegistrationServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/RegistrationServlet.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 @WebServlet(name = "RegistrationServlet", urlPatterns = "/register")
 public class RegistrationServlet extends HttpServlet {
     public static final String REGISTRATION_VIEW = "/jsp/registration.jsp";
+    public static final String USERNAME_FIELD = "username";
     public static final String EMAIL_FIELD = "email";
     public static final String PASS_FIELD = "password";
     public static final String CONF_PASS_FIELD = "confPass";
@@ -18,6 +19,7 @@ public class RegistrationServlet extends HttpServlet {
     public static final String LASTNAME_FIELD = "lastname";
 
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String username = req.getParameter(USERNAME_FIELD);
         String email = req.getParameter(EMAIL_FIELD);
         String pass = req.getParameter(PASS_FIELD);
         String confPass = req.getParameter(CONF_PASS_FIELD);
