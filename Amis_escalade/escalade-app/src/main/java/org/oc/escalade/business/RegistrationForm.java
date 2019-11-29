7fa077312e7ee100480c2b5e7e682924288bac59
mimi <lam99@hotmail.fr>
2019-11-29 11:15:52 +0100

    Now forms parameters are rerieved in a business class and no more in the servlet class

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/RegistrationForm.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/RegistrationForm.java
@@ -1,4 +1,48 @@
 package org.oc.escalade.business;
 
+import org.oc.escalade.model.User;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegistrationForm  {
+
+    private static final String USERNAME_FIELD = "username";
+    private static final String EMAIL_FIELD = "email";
+    private static final String PASS_FIELD = "password";
+    private static final String CONF_PASS_FIELD = "confPass";
+    private static final String FIRSTNAME_FIELD = "firstname";
+    private static final String LASTNAME_FIELD = "lastname";
+
+    private Map<String, String> erreurs = new HashMap<String, String>();
+
+    public Map<String, String> getErreurs(){
+        return erreurs;
+    }
+
+
+    public User registerUser(HttpServletRequest req){
+        String email = getFieldValue(req, EMAIL_FIELD );
+        String username = getFieldValue(req, USERNAME_FIELD );
+        String passsword = getFieldValue(req, PASS_FIELD );
+        String confPass = getFieldValue(req, CONF_PASS_FIELD );
+        String firstname = getFieldValue(req, FIRSTNAME_FIELD );
+        String lastname = getFieldValue(req, LASTNAME_FIELD );
+
+
+        User user = new User();
+
+
+        return user;
+    }
+
+    private static String getFieldValue(HttpServletRequest req, String field ){
+        String fieldValue = req.getParameter(field);
+        return field;
+    }
+
+    private void emailValidation(String email){
+
+    }
 }
