03fd40b8b349dfc6fb96b90fd9fc11f230150c26
mimi <lam99@hotmail.fr>
2019-12-08 21:46:56 +0100

    Add features in doGet method
    Retrieve all sites from the siteBusiness class and put them in an attribute.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/ListSitesServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/ListSitesServlet.java
@@ -1,19 +1,30 @@
 package org.oc.escalade.webapp.servlet;
 
+import org.oc.escalade.business.SitesBusiness;
+import org.oc.escalade.model.Site;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ListSitesServlet", urlPatterns = "/listSites")
 public class ListSitesServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public static final String SITE_VIEW = "/WEB-INF/jsp/listSites.jsp";
+    public static final String SITES_ATT = "sites";
+
+    protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+        SitesBusiness siteBu = new SitesBusiness();
+        List<Site> allSites = siteBu.getAllSites();
+        req.setAttribute(SITES_ATT, allSites);
+        this.getServletContext().getRequestDispatcher(SITE_VIEW).forward(req, res);
     }
 }
