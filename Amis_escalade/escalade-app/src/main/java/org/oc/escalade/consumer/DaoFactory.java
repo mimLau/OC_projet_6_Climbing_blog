77dff44908eca131f22631945c523e1d4ecb199a
mimi <lam99@hotmail.fr>
2019-12-08 16:32:11 +0100

    Create a DAO for Place enity.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/DaoFactory.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/DaoFactory.java
@@ -1,7 +1,7 @@
 package org.oc.escalade.consumer;
 
+import org.oc.escalade.consumer.impl.jpa.JpaPlaceDao;
 import org.oc.escalade.consumer.impl.jpa.JpaUserDao;
-import org.oc.escalade.model.User;
 import org.oc.escalade.utils.PersistenceManager;
 
 public class DaoFactory {
@@ -12,4 +12,9 @@ public class DaoFactory {
         return new JpaUserDao(
             PersistenceManager.getEntityManagerFactory());
         }
+    public static PlaceDao getPlaceDao() {
+
+        return new JpaPlaceDao(
+                PersistenceManager.getEntityManagerFactory());
+    }
 }
