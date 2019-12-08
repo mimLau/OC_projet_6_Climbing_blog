9ecaf88ac32cfe89d5c36b26e8c98a86a6f37a31
mimi <lam99@hotmail.fr>
2019-12-08 16:40:14 +0100

    Create  PlaceForn class.
    Content all business classes in relation with form which deal with the addition of new places.

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/PlaceForm.java
@@ -0,0 +1,58 @@
+package org.oc.escalade.business;
+
+import org.oc.escalade.consumer.DaoFactory;
+import org.oc.escalade.consumer.PlaceDao;
+import org.oc.escalade.model.Place;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+public final class PlaceForm {
+
+    private static final String COUNTRY_FIELD = "country";
+    private static final String REGION_FIELD = "region";
+    private Map<String, String> errors = new HashMap<String, String>();
+
+    private PlaceDao placeDao = DaoFactory.getPlaceDao();
+
+    public Place addPlace(HttpServletRequest req) {
+        String country = getFieldValue(req, COUNTRY_FIELD );
+        String region = getFieldValue(req, REGION_FIELD );
+
+        Place place = new Place();
+        countryValidation(country);
+        place.setCountry(country);
+        regionValidation(region);
+        place.setRegion(region);
+
+        System.out.println("Country: " + place.getCountry());
+
+        if(errors.isEmpty()) {
+            placeDao.addPlace(place);
+        }
+
+
+        return place;
+    }
+
+    private static String getFieldValue(HttpServletRequest req, String field ){
+        String fieldValue = req.getParameter(field);
+        return fieldValue;
+    }
+
+    private void countryValidation(String country) {
+        if ( country.isEmpty())
+            setError(country, "Saisissez un pays.");
+    }
+
+    private void regionValidation(String region) {
+        if ( region.isEmpty())
+            setError(region, "Saisissez une région.");
+    }
+
+    private void setError( String field, String message ) {
+        errors.put( field, message );
+    }
+
+}
