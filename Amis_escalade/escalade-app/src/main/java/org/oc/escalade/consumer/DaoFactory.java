ac59b9c9b01c482066d267e23db6a465ae6e270e
mimi <lam99@hotmail.fr>
2019-12-08 21:33:21 +0100

    New Create a DaoFactory for SiteDao.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/DaoFactory.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/DaoFactory.java
@@ -1,6 +1,7 @@
 package org.oc.escalade.consumer;
 
 import org.oc.escalade.consumer.impl.jpa.JpaPlaceDao;
+import org.oc.escalade.consumer.impl.jpa.JpaSiteDao;
 import org.oc.escalade.consumer.impl.jpa.JpaUserDao;
 import org.oc.escalade.utils.PersistenceManager;
 
@@ -12,9 +13,14 @@ public class DaoFactory {
         return new JpaUserDao(
             PersistenceManager.getEntityManagerFactory());
         }
-    public static PlaceDao getPlaceDao() {
 
+    public static PlaceDao getPlaceDao() {
         return new JpaPlaceDao(
+                PersistenceManager.getEntityManagerFactory());
+    }
+
+    public static SiteDao getSiteDao() {
+        return new JpaSiteDao(
                 PersistenceManager.getEntityManagerFactory());
     }
 }
