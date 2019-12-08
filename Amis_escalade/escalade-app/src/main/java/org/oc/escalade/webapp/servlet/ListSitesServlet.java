530b266a38a6a1378d8fdbb8c65df632801885e7
mimi <lam99@hotmail.fr>
2019-12-08 21:18:07 +0100

    New Create the ListSitesServlet.

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/ListSitesServlet.java
@@ -0,0 +1,19 @@
+package org.oc.escalade.webapp.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ListSitesServlet", urlPatterns = "/listSites")
+public class ListSitesServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
