b37e822722682004db817c295f29cdeade12d043
mimi <lam99@hotmail.fr>
2019-12-09 11:16:33 +0100

    New Enum
    Create a enumeration containing the quotation values.

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/model/Cotation.java
@@ -0,0 +1,37 @@
+package org.oc.escalade.model;
+
+public enum Cotation {
+
+    UN("1"),
+    DEUX("2"),
+    TROIS("3"),
+    QUATREA("4a"),
+    QAUTREB("4b"),
+    QUATREC("4c"),
+    CINQA("5a"),
+    CINQB("5b"),
+    CINQC("5c"),
+    SIXA("6a"),
+    SIXB("6b"),
+    SIXC("6c"),
+    SEPTA("7a"),
+    SEPTB("7b"),
+    SEPTC("7c"),
+    HUITA("8a"),
+    HUITB("8b"),
+    HUITC("8c"),
+    NEUFA("9a"),
+    NEUFAPLUS("9a+"),
+    NEUFB("9b"),
+    NEUFBPLUS("9b+");
+
+    private String value;
+
+    Cotation(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
