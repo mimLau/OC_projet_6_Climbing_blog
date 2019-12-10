aa1659508db83ddbfdb0ed233e5e014b965432ee
mimi <lam99@hotmail.fr>
2019-12-10 19:42:17 +0100

    Create interface SectorForms which will contain all business code around sectors.

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/SectorsForm.java
@@ -0,0 +1,36 @@
+package org.oc.escalade.business;
+
+
+import org.oc.escalade.consumer.DaoFactory;
+import org.oc.escalade.consumer.SectorDao;
+import org.oc.escalade.model.Sector;
+import org.oc.escalade.model.Site;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class SectorsForm {
+    private static final String NAME_FIELD = "sectorName";
+    private static final String  NB_WAYS_FIELD = "numberWay";
+    private SectorDao sectorDao = DaoFactory.getSectorDao();
+
+    public Sector addSector(HttpServletRequest req) {
+        String name = getFieldValue(req, NAME_FIELD );
+        String nbOfWays = getFieldValue(req, NB_WAYS_FIELD );
+
+        Site site = (Site) req.getServletContext().getAttribute("site");
+
+        Sector sector = new Sector();
+        sector.setName(name);
+        sector.setSite(site);
+        sector.setNbOfWays(Integer.parseInt(nbOfWays));
+
+        sector = sectorDao.addSector(sector);
+
+        return sector;
+    }
+
+    private static String getFieldValue(HttpServletRequest req, String field ){
+        String fieldValue = req.getParameter(field);
+        return fieldValue;
+    }
+}
