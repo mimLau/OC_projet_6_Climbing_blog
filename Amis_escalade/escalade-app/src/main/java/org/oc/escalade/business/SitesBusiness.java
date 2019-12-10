0a00c1a966f37299876349dc43f0c2ecc0000266
mimi <lam99@hotmail.fr>
2019-12-10 15:11:20 +0100

    New feature
    Adding a site from parameters retrieved from the form.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/SitesBusiness.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/SitesBusiness.java
@@ -5,13 +5,18 @@ import org.oc.escalade.consumer.PlaceDao;
 import org.oc.escalade.consumer.SiteDao;
 import org.oc.escalade.model.Place;
 import org.oc.escalade.model.Site;
+import org.oc.escalade.model.User;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 public final class SitesBusiness {
 
     private SiteDao siteDao = DaoFactory.getSiteDao();
     private PlaceDao placeDao = DaoFactory.getPlaceDao();
+    private static final String NAME_FIELD = "name";
+    private static final String REGION_FIELD = "region";
+    private static final String DESCRIPTION_FIELD = "description";
    // private Map<String, List<String>> countrysRegions = new HashMap<String, List<String>>();
 
     /*public Map<String, List<String>> getCountrysRegions() {
@@ -47,6 +52,30 @@ public final class SitesBusiness {
             countrysRegions.put(country, regionsBis);
         }
         return countrysRegions;
+    }
+
+    public Site addSite(HttpServletRequest req) {
+        String name = getFieldValue(req, NAME_FIELD );
+        String region = getFieldValue(req, REGION_FIELD );
+        String description = getFieldValue(req, DESCRIPTION_FIELD );
+
+        User user = (User) req.getSession().getAttribute("user");
+        Place place = placeDao.getPlaceByRegionName(region);
+
+        Site site = new Site();
+        site.setDescription(description);
+        site.setName(name);
+        site.setPlace(place);
+        site.setSiteOwner(user);
+
+        siteDao.addSite(site);
+
+        return site;
+    }
+
+    private static String getFieldValue(HttpServletRequest req, String field ){
+        String fieldValue = req.getParameter(field);
+        return fieldValue;
     }
 
 }
