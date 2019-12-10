4bf0032bf91e9c7a721a5ddd10a365428fdc0a62
mimi <lam99@hotmail.fr>
2019-12-10 13:28:09 +0100

    Retrieve SiteBusiness object to retrieve the Map with countries and theire region for the dropdown menu in addSite view.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/AddSiteServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/AddSiteServlet.java
@@ -1,21 +1,31 @@
 package org.oc.escalade.webapp.servlet;
 
+import org.oc.escalade.business.SitesBusiness;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 
 @WebServlet(name = "AddSiteServlet", urlPatterns = "/auth/addSite")
 public class AddSiteServlet extends HttpServlet {
     public static final String  ADD_SITE_VIEW = "/WEB-INF/jsp/auth/addSite.jsp";
+    public static final String SITE_ATT = "sites";
 
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
 
     }
     protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+        SitesBusiness site = new SitesBusiness();
+        Map<String, List<String>> countriesWithRegions = site.listOfCountriesRegions();
+        req.setAttribute(SITE_ATT, countriesWithRegions);
+        System.out.println("Taille de countriesWithRegions: " + countriesWithRegions.size());
+
         this.getServletContext().getRequestDispatcher(ADD_SITE_VIEW).forward(req, res);
     }
 }
