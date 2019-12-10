8f08c18907a7b21176e0cbf2d01837c1c5ae3594
mimi <lam99@hotmail.fr>
2019-12-10 13:30:03 +0100

    Add 2 interfaces:
    One to retrieve all distinct countries in Place table.
    The second to retrieve all places by country name.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/PlaceDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/PlaceDao.java
@@ -2,8 +2,12 @@ package org.oc.escalade.consumer;
 
 import org.oc.escalade.model.Place;
 
+import java.util.List;
+
 public interface PlaceDao {
 
     Place addPlace(Place place);
     Place findPlaceByCountryAndRegion(String country, String region);
+    List<String> getAllDistinctCountries();
+    List<Place> getPlaceByCountryName(String country);
 }
