669258852d64a2f56946e15c05b21162610bb212
mimi <lam99@hotmail.fr>
2019-12-13 09:26:17 +0100

    Add method and attributes to retrieve parameter in field. Create a new Way and add all this attributes to the created Way.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysForm.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysForm.java
@@ -1,4 +1,30 @@
 package org.oc.escalade.business;
 
-public class WaysForm {
+import org.oc.escalade.consumer.DaoFactory;
+import org.oc.escalade.model.Way;
+
+import javax.servlet.http.HttpServletRequest;
+
+public final class WaysForm {
+
+    private static String WAY_NAME_FIELD = "wayName";
+    private static String QUOTATION_FIELD = "quotation";
+    private static String  LENGTH_FIELD = "length";
+    //private WayDao waDao = DaoFactory.get
+
+    public Way addWay( HttpServletRequest req ) {
+        String wayName = getParamValue(req, WAY_NAME_FIELD);
+        String quotation = getParamValue(req, QUOTATION_FIELD);
+        int length = Integer.parseInt(getParamValue(req, LENGTH_FIELD));
+
+        Way way = new Way();
+
+        return way;
+    }
+
+    private static String getParamValue( HttpServletRequest req, String param ){
+        String paramValue = req.getParameter( param );
+        return paramValue;
+    }
+
 }
