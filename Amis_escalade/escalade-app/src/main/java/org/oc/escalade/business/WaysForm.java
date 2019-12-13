dcf911f6bc96ff95ef61f241e60f7db2b4020775
mimi <lam99@hotmail.fr>
2019-12-13 10:59:38 +0100

    Retrieve values field and create a new Way with this values. Add a new method to retrieve all Ratings values.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysForm.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysForm.java
@@ -1,25 +1,36 @@
 package org.oc.escalade.business;
 
 import org.oc.escalade.consumer.DaoFactory;
+import org.oc.escalade.consumer.WayDao;
+import org.oc.escalade.model.Rating;
 import org.oc.escalade.model.Way;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 public final class WaysForm {
 
     private static String WAY_NAME_FIELD = "wayName";
-    private static String QUOTATION_FIELD = "quotation";
+    private static String RATING_FIELD = "rating";
     private static String  LENGTH_FIELD = "length";
-    //private WayDao waDao = DaoFactory.get
+    private WayDao wayDao = DaoFactory.getWayDao();
 
     public Way addWay( HttpServletRequest req ) {
         String wayName = getParamValue(req, WAY_NAME_FIELD);
-        String quotation = getParamValue(req, QUOTATION_FIELD);
-        int length = Integer.parseInt(getParamValue(req, LENGTH_FIELD));
+        String rating = getParamValue(req, RATING_FIELD);
+        long length = Long.parseLong(getParamValue(req, LENGTH_FIELD));
 
         Way way = new Way();
+        way.setName(wayName);
+        way.setRating(rating);
+        way.setLength(length);
+        wayDao.addWay(way);
 
         return way;
+    }
+
+    public List<String> getAllRatings() {
+       return  Rating.getAllRtingsValues();
     }
 
     private static String getParamValue( HttpServletRequest req, String param ){
