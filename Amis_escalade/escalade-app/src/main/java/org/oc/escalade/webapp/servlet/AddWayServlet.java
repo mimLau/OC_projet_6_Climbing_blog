9ac3a545af8025984a476366ae4550a5ac826b9e
mimi <lam99@hotmail.fr>
2019-12-13 11:02:59 +0100

    Dopost: Retrieve the way added from the form. Doget: retrieve ratings values and put them in an attribute.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/AddWayServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/AddWayServlet.java
@@ -1,20 +1,39 @@
 package org.oc.escalade.webapp.servlet;
 
+import org.oc.escalade.business.WaysForm;
+import org.oc.escalade.model.Rating;
+import org.oc.escalade.model.Site;
+import org.oc.escalade.model.Way;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.WatchService;
+import java.util.List;
 
 @WebServlet(name = "AddWayServlet", urlPatterns = "/auth/addWay")
 public class AddWayServlet extends HttpServlet {
     public static final String  ADD_WAY_VIEW = "/WEB-INF/jsp/auth/addWay.jsp";
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    public static final String  SHOW_SECTOR_PAGE = "/showSector?id=";
+    public static final String RATINGS_ATT = "ratings";
+    public static final String SITE_ATT = "site";
+    public static final String WAY_ATT = "way";
 
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        WaysForm form = new WaysForm();
+        Way way = form.addWay(req);
+        //req.getServletContext().setAttribute(WAY_ATT, way);
+        Site site = (Site) req.getServletContext().getAttribute(SITE_ATT);
+        res.sendRedirect(req.getContextPath() + SHOW_SECTOR_PAGE + site.getId());
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        WaysForm form = new WaysForm();
+        List<String> ratingValues = form.getAllRatings();
+        req.getServletContext().setAttribute(RATINGS_ATT, ratingValues);
         this.getServletContext().getRequestDispatcher(ADD_WAY_VIEW).forward(req, res);
 
     }
