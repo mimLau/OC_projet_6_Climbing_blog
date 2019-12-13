c16bd70db09ce340cf5ad600e0d784c6d19ef8fe
mimi <lam99@hotmail.fr>
2019-12-13 11:07:39 +0100

    Add new interface to retrieve all ways existing in base.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/WayDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/WayDao.java
@@ -2,6 +2,9 @@ package org.oc.escalade.consumer;
 
 import org.oc.escalade.model.Way;
 
+import java.util.List;
+
 public interface WayDao {
     Way addWay(Way way);
+    List<Way> getAllWays();
 }
