de25eef438e43f93d1f04cd13486a26c51d44134
mimi <lam99@hotmail.fr>
2019-12-14 10:38:37 +0100

    Refactor
    RegistrationForm-> RegistrationManager

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/RegistrationServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/webapp/servlet/RegistrationServlet.java
@@ -1,15 +1,13 @@
 package org.oc.escalade.webapp.servlet;
 
-import org.oc.escalade.business.RegistrationForm;
+import org.oc.escalade.business.RegistrationManager;
 import org.oc.escalade.model.User;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.plaf.PanelUI;
 import java.io.IOException;
 
 @WebServlet(name = "RegistrationServlet", urlPatterns = "/register")
@@ -21,7 +19,7 @@ public class RegistrationServlet extends HttpServlet {
 
 
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
-        RegistrationForm form = new RegistrationForm();
+        RegistrationManager form = new RegistrationManager();
         User user = form.registerUser(req);
 
         req.setAttribute(FORM_ATT, form);
