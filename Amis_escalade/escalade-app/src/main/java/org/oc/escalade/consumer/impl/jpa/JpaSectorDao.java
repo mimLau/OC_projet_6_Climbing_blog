472bbe6bf3122f05bab67e5299d29a45bae8b69f
mimi <lam99@hotmail.fr>
2019-12-14 10:41:14 +0100

    Add new method
    findSectorById

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/impl/jpa/JpaSectorDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumer/impl/jpa/JpaSectorDao.java
@@ -2,28 +2,45 @@ package org.oc.escalade.consumer.impl.jpa;
 
 import org.oc.escalade.consumer.SectorDao;
 import org.oc.escalade.model.Sector;
+import org.oc.escalade.model.Site;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 public class JpaSectorDao implements SectorDao {
     private EntityManagerFactory emf;
 
-    public JpaSectorDao(EntityManagerFactory emf){
+    public JpaSectorDao( EntityManagerFactory emf ){
         this.emf=emf;
     }
 
     @Override
-    public Sector addSector(Sector sector) {
+    public Sector addSector( Sector sector ) {
         final EntityManager em = emf.createEntityManager();
         EntityTransaction t = em.getTransaction();
         try {
             t.begin();
-            em.persist(sector);
+            em.persist( sector );
             t.commit();
         }finally {
-            if(t.isActive()) t.rollback();
+            if( t.isActive() ) t.rollback();
+        }
+        return sector;
+    }
+
+    @Override
+    public Sector findSectorById( Long id ) {
+        final EntityManager em = emf.createEntityManager();
+        Sector sector = new Sector();
+
+        try {
+            Query query = em.createQuery("SELECT s FROM Sector AS s LEFT JOIN FETCH s.ways WHERE s.id= :id ");
+            query.setParameter("id", id);
+            sector = ( Sector) query.getSingleResult();
+        } finally {
+            em.close();
         }
         return sector;
     }
