89cdd10a2ffe6b12fd0a7a9425d36b1a45cb23ca
mimi <lam99@hotmail.fr>
2019-12-16 11:37:18 +0100

    Refactor
    consumer -> consumers
    model -> models

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowSectorServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowSectorServlet.java
@@ -1,4 +1,7 @@
-package org.oc.escalade.webapp.servlet;
+package org.oc.escalade.controllers.servlets;
+
+import org.oc.escalade.business.SectorsManager;
+import org.oc.escalade.models.Sector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +13,15 @@ import java.io.IOException;
 @WebServlet(name = "ShowSecteurServlet", urlPatterns = "/showSector")
 public class ShowSectorServlet extends HttpServlet {
     public static final String SHOW_SECTOR_VIEW = "/WEB-INF/jsp/showSector.jsp";
+    public static final String SECTOR_ATT = "sector";
     protected void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
     }
 
     protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
-        this.getServletContext().getRequestDispatcher(SHOW_SECTOR_VIEW).forward(req, res);
+        SectorsManager sectorsManager = new SectorsManager();
+        Sector sector = sectorsManager.getSectorById( req );
+        req.getServletContext().setAttribute( SECTOR_ATT, sector );
+        this.getServletContext().getRequestDispatcher( SHOW_SECTOR_VIEW ).forward(req, res);
     }
 }
