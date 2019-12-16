55960ec1e41f7b16bc959ac617435d81fb2c1a18
mimi <lam99@hotmail.fr>
2019-12-16 11:27:14 +0100

    Refactor
    consumer -> consumers
    model -> models

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaWayDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaWayDao.java
@@ -1,7 +1,7 @@
-package org.oc.escalade.consumer.impl.jpa;
+package org.oc.escalade.consumers.impl.jpa;
 
-import org.oc.escalade.consumer.WayDao;
-import org.oc.escalade.model.Way;
+import org.oc.escalade.consumers.WayDao;
+import org.oc.escalade.models.Way;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -47,6 +47,21 @@ public class JpaWayDao implements WayDao {
         }
 
         return allWays;
+    }
+
+    @Override
+    public Way findWayById( Long id ) {
+        final EntityManager em = emf.createEntityManager();
+        Way way = new Way();
+
+        try {
+            Query query = em.createQuery("SELECT w FROM Way AS w LEFT JOIN FETCH w.lengths WHERE w.id= :id ");
+            query.setParameter("id", id);
+            way = ( Way) query.getSingleResult();
+        } finally {
+            em.close();
+        }
+        return way;
     }
 
 }
