9fcf8e88a50c1aea7839d7f831bce55a15a4376a
mimi <lam99@hotmail.fr>
2019-12-17 21:29:20 +0100

    Set a sector  to the entity Way when created.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysManager.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/WaysManager.java
@@ -1,8 +1,10 @@
 package org.oc.escalade.business;
 
 import org.oc.escalade.consumers.DaoFactory;
+import org.oc.escalade.consumers.SectorDao;
 import org.oc.escalade.consumers.WayDao;
 import org.oc.escalade.models.Rating;
+import org.oc.escalade.models.Sector;
 import org.oc.escalade.models.Way;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,18 +17,22 @@ public final class WaysManager {
     private static String  LENGTH_FIELD = "length";
     private static final String ID_PARAM_NAME = "id";
     private WayDao wayDao = DaoFactory.getWayDao();
+    private SectorDao sectorDao = DaoFactory.getSectorDao();
 
     public Way addWay( HttpServletRequest req ) {
         String wayName = getParamValue(req, WAY_NAME_FIELD);
         String rating = getParamValue(req, RATING_FIELD);
         long length = Long.parseLong(getParamValue(req, LENGTH_FIELD));
+        Sector sector = ( Sector ) req.getServletContext().getAttribute("sector" );
 
         Way way = new Way();
         way.setName(wayName);
         way.setRating(rating);
         way.setLength(length);
+        way.setSector(sector);
         wayDao.addWay(way);
 
+        sectorDao.updateNbOfWays(sector.getId());
         return way;
     }
 
