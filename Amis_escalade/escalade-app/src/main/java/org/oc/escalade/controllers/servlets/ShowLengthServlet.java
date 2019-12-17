c978129126ff3a7118feb44c2b640a67bf773311
mimi <lam99@hotmail.fr>
2019-12-17 21:24:03 +0100

    Rectify UrlPatterns Complete the doGet method.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowLengthServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowLengthServlet.java
@@ -1,5 +1,8 @@
 package org.oc.escalade.controllers.servlets;
 
+import org.oc.escalade.business.LengthsManager;
+import org.oc.escalade.models.Length;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "ShowLengthServlet", urlPatterns = "/WEB-INF/jsp//showLength")
+@WebServlet(name = "ShowLengthServlet", urlPatterns = "/showLength")
 public class ShowLengthServlet extends HttpServlet {
-    public static final String SHOW_LENGTH_VIEW = "showLength.jsp";
+    public static final String SHOW_LENGTH_VIEW = "/WEB-INF/jsp/showLength.jsp";
+    public static final String LENGTH_ATT = "length";
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-    this.getServletContext().getRequestDispatcher(SHOW_LENGTH_VIEW).forward( req, res );
+        LengthsManager lengthsManager = new LengthsManager();
+        Length length = lengthsManager.getLengthById(req);
+        req.getServletContext().setAttribute( LENGTH_ATT, length);
+        this.getServletContext().getRequestDispatcher(SHOW_LENGTH_VIEW).forward( req, res );
     }
 }
