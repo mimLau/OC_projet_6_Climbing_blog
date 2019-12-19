d870d50001331d8687b5ded60c0909a44b0b6b86
mimi <lam99@hotmail.fr>
2019-12-19 21:43:26 +0100

    Add method to retrieve comments from the form which is in showSite view.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowSiteServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ShowSiteServlet.java
@@ -1,6 +1,8 @@
 package org.oc.escalade.controllers.servlets;
 
+import org.oc.escalade.business.CommentsManager;
 import org.oc.escalade.business.SitesManager;
+import org.oc.escalade.models.Comment;
 import org.oc.escalade.models.Site;
 
 import javax.servlet.ServletException;
@@ -13,10 +15,17 @@ import java.io.IOException;
 @WebServlet(name = "ShowSiteServlet", urlPatterns = "/showSite")
 public class ShowSiteServlet extends HttpServlet {
     public static final String  SHOW_SITE_VIEW = "/WEB-INF/jsp/showSite.jsp";
+    public static final String  SHOW_SITE_PAGE = "/showSite?id=";
     public static final String SITE_ATT = "site";
+    public static final String COMMENT_ATT = "comment";
 
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+        CommentsManager commentsManager = new CommentsManager();
+        commentsManager.addComment( req );
+        req.getServletContext().getAttribute( COMMENT_ATT );
+        Site site = (Site) req.getServletContext().getAttribute( SITE_ATT );
 
+        res.sendRedirect(req.getContextPath() + SHOW_SITE_PAGE + site.getId());
     }
 
     protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
@@ -24,6 +33,6 @@ public class ShowSiteServlet extends HttpServlet {
         Site site = sitesManager.getSiteById( req );
         req.getServletContext().setAttribute( SITE_ATT, site );
 
-        req.getRequestDispatcher( SHOW_SITE_VIEW ).forward( req, res );
+        this.getServletContext().getRequestDispatcher( SHOW_SITE_VIEW ).forward( req, res );
     }
 }
