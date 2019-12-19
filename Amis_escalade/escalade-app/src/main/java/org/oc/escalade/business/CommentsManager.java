dd3f8ba412ff5a64ee13889bd53c6c6cfc65d282
mimi <lam99@hotmail.fr>
2019-12-19 21:42:23 +0100

    Create CommentManager with addComment method

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/CommentsManager.java
@@ -0,0 +1,45 @@
+package org.oc.escalade.business;
+
+import org.oc.escalade.consumers.CommentDao;
+import org.oc.escalade.consumers.DaoFactory;
+import org.oc.escalade.models.Comment;
+import org.oc.escalade.models.Site;
+import org.oc.escalade.models.User;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CommentsManager {
+    private CommentDao commentDao = DaoFactory.getCommentDao();
+    private final static String COMMENT_CONTENTS_FIELD = "comment_contents";
+    private static final String ID_PARAMETER_NAME = "id";
+    private static final String SITE_ATT =  "site";
+
+    public Comment addComment( HttpServletRequest req ) {
+        String commentContents = getParameterValue( req, COMMENT_CONTENTS_FIELD );
+        User user = (User) req.getSession().getAttribute("user");
+        Site site = (Site) req.getServletContext().getAttribute("site");
+        Long siteId = site.getId();
+        req.getServletContext().setAttribute( SITE_ATT, site );
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+        Date date = new Date();
+
+        Comment comment = new Comment();
+        comment.setContents( commentContents );
+        comment.setCommentOwner( user );
+        comment.setSiteComment( site );
+        comment.setDate( date );
+
+        commentDao.addComment( comment );
+
+        return comment;
+    }
+
+    private static String getParameterValue( HttpServletRequest req, String param ){
+        String paramValue = req.getParameter( param );
+        return paramValue;
+    }
+}
