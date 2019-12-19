f301086b74167adfa18f055f4ea0e438fe027012
mimi <lam99@hotmail.fr>
2019-12-19 19:37:27 +0100

    Add addComment method.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaCommentDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaCommentDao.java
@@ -1,14 +1,30 @@
 package org.oc.escalade.consumers.impl.jpa;
 
 import org.oc.escalade.consumers.CommentDao;
+import org.oc.escalade.models.Comment;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 
 public class JpaCommentDao implements CommentDao {
     private EntityManagerFactory emf;
 
     public JpaCommentDao(EntityManagerFactory emf) {
         this.emf = emf;
+    }
+
+    @Override
+    public Comment addComment(Comment comment) {
+       final  EntityManager em = emf.createEntityManager();
+        EntityTransaction t = em.getTransaction();
+        try {
+            t.begin();
+            em.persist(comment);
+            t.commit();
+        } finally {
+            if( t.isActive() ) t.rollback();
+        }
+        return comment;
     }
 }
