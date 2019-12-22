5b90cc826020a0e65f317d39cc060356b1a6589e
mimi <lam99@hotmail.fr>
2019-12-22 12:24:29 +0100

    Create new servlet, AddCommentServlet.

---
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/AddCommentServlet.java
@@ -0,0 +1,19 @@
+package org.oc.escalade.controllers.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "AddCommentServlet", urlPatterns = "/auth/addComment")
+public class AddCommentServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
