d26cdafce6559efaabd7455bb1c741e451fd65cf
mimi <lam99@hotmail.fr>
2019-12-22 12:29:53 +0100

    Retrieve classe ComemntsManager and the method addComment to add a comment. Put the creating Comment in a Attribute. Retrieve the Site from the servletContex.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/AddCommentServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/AddCommentServlet.java
@@ -1,5 +1,8 @@
 package org.oc.escalade.controllers.servlets;
 
+import org.oc.escalade.business.CommentsManager;
+import org.oc.escalade.models.Site;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +12,20 @@ import java.io.IOException;
 
 @WebServlet(name = "AddCommentServlet", urlPatterns = "/auth/addComment")
 public class AddCommentServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public static final String COMMENT_ATT = "comment";
+    public static final String  SHOW_SITE_PAGE = "/showSite?id=";
+    public static final String SITE_ATT = "site";
 
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        CommentsManager commentsManager = new CommentsManager();
+        commentsManager.addComment( req );
+        req.getServletContext().getAttribute( COMMENT_ATT );
+        Site site = (Site) req.getServletContext().getAttribute( SITE_ATT );
+
+        res.sendRedirect(req.getContextPath() + SHOW_SITE_PAGE + site.getId());
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
     }
 }
