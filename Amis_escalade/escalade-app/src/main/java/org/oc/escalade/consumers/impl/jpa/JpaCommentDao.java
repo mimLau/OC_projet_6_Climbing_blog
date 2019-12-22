412fc8cffeed76810e33e9a08fd3bd4f039ff75b
mimi <lam99@hotmail.fr>
2019-12-22 12:20:42 +0100

    Add method getCommentsBySiteId, deleteComById,

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaCommentDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaCommentDao.java
@@ -2,10 +2,14 @@ package org.oc.escalade.consumers.impl.jpa;
 
 import org.oc.escalade.consumers.CommentDao;
 import org.oc.escalade.models.Comment;
+import org.oc.escalade.models.Site;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JpaCommentDao implements CommentDao {
     private EntityManagerFactory emf;
@@ -26,5 +30,36 @@ public class JpaCommentDao implements CommentDao {
             if( t.isActive() ) t.rollback();
         }
         return comment;
+    }
+
+    @Override
+    public List<Comment> getCommentsBySiteId(Site site) {
+        final  EntityManager em = emf.createEntityManager();
+        List <Comment> comments = new ArrayList<Comment>();
+
+        try {
+            Query query = em.createQuery("SELECT c FROM Comment AS c WHERE c.siteComment= :site");
+            query.setParameter("site", site );
+            comments = (List<Comment>) query.getResultList();
+        } finally {
+            em.close();
+        }
+        return comments;
+    }
+
+    @Override
+    public void deleteComById(Long id) {
+        final EntityManager em = emf.createEntityManager();
+        final EntityTransaction t = em.getTransaction();
+        try {
+            t.begin();
+            Query query = em.createQuery("DELETE FROM Comment c WHERE c.id = :id");
+            query.setParameter("id", id);
+            query.executeUpdate();
+            t.commit();
+        } finally {
+            if( t.isActive() ) t.rollback();
+        }
+        em.close();
     }
 }
