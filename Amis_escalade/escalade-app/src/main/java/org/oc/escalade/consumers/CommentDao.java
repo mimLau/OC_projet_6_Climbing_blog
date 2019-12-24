8a40e9ec2d1dd9bc3b7a550de358ab02ffaaa4aa
mimi <lam99@hotmail.fr>
2019-12-24 09:48:44 +0100

    Add editedDate in updateComment params.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/CommentDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/CommentDao.java
@@ -3,6 +3,7 @@ package org.oc.escalade.consumers;
 import org.oc.escalade.models.Comment;
 import org.oc.escalade.models.Site;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CommentDao {
@@ -10,5 +11,5 @@ public interface CommentDao {
     Comment addComment( Comment comment );
     List<Comment> getCommentsBySiteId( Site site );
     void deleteComById( Long id );
-    void updateCommentById( Long id, String content );
+    void updateCommentById( Long id, String newContent, Date editedDate);
 }
