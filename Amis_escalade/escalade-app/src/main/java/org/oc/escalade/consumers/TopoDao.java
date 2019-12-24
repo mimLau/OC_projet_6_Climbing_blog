0657e5a4a0c42168a305b1f18f69ef576d034453
mimi <lam99@hotmail.fr>
2019-12-24 16:09:52 +0100

    Add interfaces

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/TopoDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/TopoDao.java
@@ -1,4 +1,13 @@
 package org.oc.escalade.consumers;
 
+import org.oc.escalade.models.Topo;
+
+import java.util.List;
+
 public interface TopoDao {
+
+    List<Topo> getAllToposByUserId( Long userId );
+    List<Topo> getAllTopos();
+    Topo addTopo( Topo topo );
+    void updateTopoState( Long topoId, boolean borrowed );
 }
