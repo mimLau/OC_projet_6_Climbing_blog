829eb9118a74039c01e6b6783f861b58f1604eda
mimi <lam99@hotmail.fr>
2020-01-02 10:58:40 +0100

    Controller for manging the display of all topos.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ListAllToposServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ListAllToposServlet.java
@@ -10,10 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-@WebServlet(name = "ListAllToposServlet", urlPatterns = "/listAllTopos")
+@WebServlet(name = "ListAllToposServlet", urlPatterns = "/auth/listAllTopos")
 public class ListAllToposServlet extends HttpServlet {
-    public static final String  LIST_TOPOS_VIEW = "/WEB-INF/jsp/listAllTopos.jsp";
+    public static final String  LIST_TOPOS_VIEW = "/WEB-INF/jsp/auth/listAllTopos.jsp";
     public static final String TOPOS_ATT = "topos";
 
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
@@ -22,7 +23,7 @@ public class ListAllToposServlet extends HttpServlet {
 
     protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
         ToposManager toposManager = new ToposManager();
-        List<Topo> topos = toposManager.getAllTopos();
+        Map<Topo, Boolean> topos = toposManager.getAllTopos( req );
         req.setAttribute( TOPOS_ATT, topos );
         this.getServletContext().getRequestDispatcher( LIST_TOPOS_VIEW ).forward( req, res );
     }
