8f1640e69b78e698b3555fcb990cffb678969422
mimi <lam99@hotmail.fr>
2020-01-02 10:37:02 +0100

    Add new attribute, userLender, the topo owner.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/models/Booking.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/models/Booking.java
@@ -13,12 +13,55 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     private Topo topo;
 
     @ManyToOne
     private User userRequest;
 
+    @ManyToOne
+    private User userLender;
+
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Topo getTopo() {
+        return topo;
+    }
+
+    public void setTopo(Topo topo) {
+        this.topo = topo;
+    }
+
+    public User getUserRequest() {
+        return userRequest;
+    }
+
+    public void setUserRequest(User userRequest) {
+        this.userRequest = userRequest;
+    }
+
+    public User getUserLender() {
+        return userLender;
+    }
+
+    public void setUserLender(User userLender) {
+        this.userLender = userLender;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
