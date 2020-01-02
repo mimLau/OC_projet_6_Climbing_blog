9314d2b2becf7880d6f630871b2042f186669992
mimi <lam99@hotmail.fr>
2020-01-02 10:51:18 +0100

    Modify method updateTag, add a boolean in param. Add new method, getSiteBySearchparams

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaSiteDao.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/consumers/impl/jpa/JpaSiteDao.java
@@ -1,12 +1,10 @@
 package org.oc.escalade.consumers.impl.jpa;
 
 import org.oc.escalade.consumers.SiteDao;
+import org.oc.escalade.models.Place;
 import org.oc.escalade.models.Site;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Query;
+import javax.persistence.*;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
@@ -49,7 +47,7 @@ public class JpaSiteDao implements SiteDao {
     }
 
     @Override
-    public Site findSiteById(long id) {
+    public Site findSiteById(Long id) {
         final EntityManager em = emf.createEntityManager();
         Site site = new Site();
 
@@ -61,7 +59,6 @@ public class JpaSiteDao implements SiteDao {
             Query  query2 = em.createQuery("SELECT DISTINCT s FROM Site AS s LEFT JOIN FETCH s.comments WHERE  s IN :site");
             query2.setParameter("site", site);
             site = (Site) query2.getSingleResult();
-
         } finally {
             em.close();
         }
@@ -90,14 +87,15 @@ public class JpaSiteDao implements SiteDao {
         }
 
     @Override
-    public void updateTag(Long sitId) {
+    public void updateTag( Long sitId, Boolean tag ) {
         final EntityManager em = emf.createEntityManager();
         EntityTransaction t = em.getTransaction();
 
         try {
             t.begin();
-            Query query = em.createQuery("UPDATE Site s SET s.tagged = true WHERE s.id= :id");
+            Query query = em.createQuery("UPDATE Site s SET s.tagged= :tag WHERE s.id= :id");
             query.setParameter("id", sitId);
+            query.setParameter("tag", tag);
 
             query.executeUpdate();
             t.commit();
@@ -114,16 +112,87 @@ public class JpaSiteDao implements SiteDao {
         final EntityManager em = emf.createEntityManager();
         List<Site> sites = new ArrayList<>();
 
+        Query query = em.createQuery("SELECT s FROM Site AS s  WHERE s.place.id= :id ");
+        query.setParameter("id", placeId);
+
         try {
-            Query query = em.createQuery("SELECT s FROM Site AS s  WHERE s.place.id= :id ");
-            query.setParameter("id", placeId);
             sites = (List<Site>) query.getResultList();
-
-
-
         } finally {
             em.close();
         }
         return sites;
+    }
+
+    @Override
+    public List<Site> getSiteBySearchParams( Place place, int nbOfSectors, Boolean tagged, List<Long> siteIds ) {
+        final EntityManager em = emf.createEntityManager();
+        List<Site> sites = new ArrayList<>();
+
+        try {
+
+            if( place != null && nbOfSectors != 0 ) {
+
+                Query query = em.createQuery("SELECT s FROM Site AS s  WHERE s.place= :place AND s.nbOfSectors= :nbOfSectors AND s.tagged= :tagged");
+                query.setParameter("place", place);
+                query.setParameter("nbOfSectors", nbOfSectors);
+                query.setParameter("tagged", tagged);
+
+                try {
+                    sites = (List<Site>) query.getResultList();
+                } catch( NoResultException noResultE) {
+                    sites = null;
+                }
+
+            } else if ( place == null && nbOfSectors != 0 ) {
+
+                Query query = em.createQuery("SELECT s FROM Site AS s  WHERE s.nbOfSectors= :nbOfSectors AND s.tagged= :tagged");
+                query.setParameter("nbOfSectors", nbOfSectors);
+                query.setParameter("tagged", tagged);
+
+                try {
+                    sites = (List<Site>) query.getResultList();
+                } catch( NoResultException noResultE) {
+                    sites = null;
+                }
+            } else if ( place != null && nbOfSectors == 0 ) {
+
+                Query query = em.createQuery("SELECT s FROM Site AS s  WHERE s.place= :place AND s.tagged= :tagged");
+                query.setParameter("place", place);
+                query.setParameter("tagged", tagged);
+
+                try {
+                    sites = (List<Site>) query.getResultList();
+                } catch (NoResultException noResultE) {
+                    sites = null;
+                }
+            }
+
+            if( !siteIds.isEmpty() ) {
+                String stringQuery = "SELECT s FROM Site AS s  WHERE s.id IN :ids";
+
+                Query query2;
+                if( sites.isEmpty() ) {
+                   query2  = em.createQuery(stringQuery);
+                   query2.setParameter("ids", siteIds);
+
+                } else {
+                    query2 = em.createQuery(stringQuery + " AND s IN :sites");
+
+                    query2.setParameter("ids", siteIds);
+                    query2.setParameter("sites", sites);
+                }
+
+                try {
+                    sites = (List<Site>) query2.getResultList();
+                } catch (NoResultException noResultE) {
+                    sites = null;
+                }
+            }
+        } finally {
+            em.close();
+        }
+
+        return sites;
+
     }
 }
