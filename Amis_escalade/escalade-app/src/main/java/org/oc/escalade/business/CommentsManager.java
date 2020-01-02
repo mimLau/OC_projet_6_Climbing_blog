c637e8ce67d85da899e2fb2a4b3a4db914884600
mimi <lam99@hotmail.fr>
2020-01-02 14:39:42 +0100

    Delete method getParameterValue. Use the static method.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/CommentsManager.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/business/CommentsManager.java
@@ -5,6 +5,7 @@ import org.oc.escalade.consumers.DaoFactory;
 import org.oc.escalade.models.Comment;
 import org.oc.escalade.models.Site;
 import org.oc.escalade.models.User;
+import org.oc.escalade.utils.RetrieveParamValue;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.DateFormat;
@@ -17,11 +18,12 @@ public class CommentsManager {
     private final static String COMMENT_EDIT_CONTENTS_FIELD = "comment_edit_contents";
     private final static String ID_COMMENT_PARAM = "id";
     private static final String SITE_ATT =  "site";
+    private final static String USER_SESSION = "user";
 
     public Comment addComment( HttpServletRequest req ) {
-        String commentContents = getParameterValue( req, COMMENT_CONTENTS_FIELD );
-        User user = (User) req.getSession().getAttribute("user");
-        Site site = (Site) req.getServletContext().getAttribute("site");
+        String commentContents = RetrieveParamValue.getParameterValue( req, COMMENT_CONTENTS_FIELD );
+        User user = (User) req.getSession().getAttribute( USER_SESSION );
+        Site site = (Site) req.getServletContext().getAttribute( USER_SESSION );
         req.getServletContext().setAttribute( SITE_ATT, site );
 
         DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
@@ -39,19 +41,15 @@ public class CommentsManager {
     }
 
     public void deleteComment( HttpServletRequest req ) {
-        Long commentId = Long.parseLong(getParameterValue( req,  ID_COMMENT_PARAM ));
+        Long commentId = Long.parseLong(RetrieveParamValue.getParameterValue( req,  ID_COMMENT_PARAM ));
         commentDao.deleteComById(commentId);
     }
 
     public void updateComment( HttpServletRequest req ) {
-        Long commentId = Long.parseLong(getParameterValue( req,  ID_COMMENT_PARAM ));
-        String commentContents = getParameterValue(  req, COMMENT_EDIT_CONTENTS_FIELD );
+        Long commentId = Long.parseLong(RetrieveParamValue.getParameterValue( req,  ID_COMMENT_PARAM ));
+        String commentContents = RetrieveParamValue.getParameterValue(  req, COMMENT_EDIT_CONTENTS_FIELD );
         Date editedDate = new Date();
         commentDao.updateCommentById( commentId, commentContents , editedDate );
     }
 
-    private static String getParameterValue( HttpServletRequest req, String param ){
-        String paramValue = req.getParameter( param );
-        return paramValue;
-    }
 }
