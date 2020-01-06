5be841002915518f037b7eeb1dd65b935faeb294
mimi <lam99@hotmail.fr>
2020-01-06 12:34:43 +0100

    Put the same value "sites" to All_SITES_ATT and SEARCHED_SITES_ATT. Add attribute "list" that contains true value if attribute "sites" contains list of all sites. Add attribute "tags" that contains values of Tag enumeration. Add attribute "nbSectorMax" that contains the max sector a site have.
    Remove all Attributes that contain Selected option value of the search banner.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ListSitesServlet.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/controllers/servlets/ListSitesServlet.java
@@ -3,6 +3,7 @@ package org.oc.escalade.controllers.servlets;
 import org.oc.escalade.business.SitesManager;
 import org.oc.escalade.models.Site;
 import org.oc.escalade.utils.RatingManager;
+import org.oc.escalade.utils.TagsManager;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,28 +17,44 @@ import java.util.Map;
 @WebServlet(name = "ListSitesServlet", urlPatterns = "/listSites")
 public class ListSitesServlet extends HttpServlet {
     public static final String SITE_VIEW = "/WEB-INF/jsp/listSites.jsp";
-    public static final String ALL_SITES_ATT = "all_sites";
-    public static final String SEARCHED_SITES_ATT = "searched_sites";
+    public static final String ALL_SITES_ATT = "sites";
+    public static final String SEARCHED_SITES_ATT = "sites";
     public static final String COUNTRY_REGION_ATT = "countryRegions";
     public static final String RATINGS_ATT = "ratings";
+    public static final String TAGS_ATT = "tags";
+    public static final String NB_SECTOR_MAX_ATT = "nbSectorMax";
+    public static final String LIST_ATT = "list";
 
     protected void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
         SitesManager sitesManager = new SitesManager();
         List<Site> searchedSites = sitesManager.searchSite( req );
 
         req.setAttribute(SEARCHED_SITES_ATT, searchedSites);
+        req.setAttribute(LIST_ATT, false);
         this.getServletContext().getRequestDispatcher(SITE_VIEW).forward(req, res);
     }
 
     protected void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
         List<String> ratingValues = RatingManager.getAllRatings();
-        req.getServletContext().setAttribute(RATINGS_ATT, ratingValues);
+        Map<String, Boolean> tags = TagsManager.getAllTags();
 
         SitesManager sitesManager = new SitesManager();
         List<Site> allSites = sitesManager.getAllSites();
         Map<String, List<String>> countriesWithRegions = sitesManager.listOfCountriesRegions();
+        int nbSectorMax = sitesManager.getSectorNbMax();
+
         req.getServletContext().setAttribute(COUNTRY_REGION_ATT, countriesWithRegions);
+        req.getServletContext().setAttribute(RATINGS_ATT, ratingValues);
+        req.getServletContext().setAttribute(TAGS_ATT, tags);
         req.setAttribute(ALL_SITES_ATT, allSites);
+        req.setAttribute(LIST_ATT, true);
+        req.getServletContext().setAttribute(NB_SECTOR_MAX_ATT, nbSectorMax);
+
+        req.getServletContext().removeAttribute("chosen_region");
+        req.getServletContext().removeAttribute("chosen_tag");
+        req.getServletContext().removeAttribute("chosen_sectorNb");
+        req.getServletContext().removeAttribute("chosen_rating");
+
         this.getServletContext().getRequestDispatcher(SITE_VIEW).forward(req, res);
     }
 }
