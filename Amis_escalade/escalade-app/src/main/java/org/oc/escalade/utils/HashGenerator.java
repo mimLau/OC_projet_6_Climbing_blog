ba0484db88d48e9c8307f5fd9dde241597af8aa4
mimi <lam99@hotmail.fr>
2020-01-08 23:09:14 +0100

    Add new hash generator using h256.

--- Amis_escalade/escalade-app/src/main/java/org/oc/escalade/utils/HashGenerator.java
+++ Amis_escalade/escalade-app/src/main/java/org/oc/escalade/utils/HashGenerator.java
@@ -1,5 +1,7 @@
 package org.oc.escalade.utils;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -25,4 +27,35 @@ public class HashGenerator {
         }
         return cryptoText;
     }
+
+
+    public final static String toHexString(String input) throws NoSuchAlgorithmException {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, getSHA(input));
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 32)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
+    }
+
+
+
+    private static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+
 }
